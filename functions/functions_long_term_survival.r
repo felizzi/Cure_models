c200eee1b8c38708a37714d9ce7c1c5e3a206202
Felizzi <felizzif@emea.roche.com>
2018-08-17 15:35:10 +0200

    removal of the SAS header

--- functions/functions_long_term_survival.r
+++ functions/functions_long_term_survival.r
@@ -1,36 +1,3 @@
-#/*******************************************************************************
-#* XXXXXXXX - (short program description)
-#********************************************************************************
-#* Project : cd66600a.pbe
-#* Study : m66601g.pbe
-#* Author : Felizzi, Federico {MGPM~Basel} 
-#* SAS version : 9.2
-#* $Id: sasheader.sas,v 1.6 2014-01-31 16:39:43+01 ravenhas Exp $
-#* $Source: /opt/BIOSTAT/razordb/RAZOR_UNIVERSE/DOMAIN_01/acp/Archive/RZ_VCS/acp/sas/sasheader.sas,v $
-#********************************************************************************
-#* (full program description)
-#*
-#*
-#*
-#*
-#********************************************************************************
-#* Inputs :
-#*
-#*
-#* Outputs :
-#*
-#*
-#* Called macros :
-#*
-#*
-#----- for macros only -------
-#********************************************************************************
-#* Positional parameters :
-#*
-#* Named parameters :
-#*
-#----- end for macros only -----
-#*******************************************************************************/
 
   #series of functions that will be used by the fitting program 
   
